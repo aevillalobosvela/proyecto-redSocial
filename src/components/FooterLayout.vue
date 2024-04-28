7f87ff1fbaf5b7bb3bc98731168548d18fc60a40
Alvaro Villalobos Vela <vivealed@gmail.com>
2024-04-28 11:33:03 -0400

    delete 1 file and update 2 files

---
+++ src/components/FooterLayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="mt-5">
+    <footer class="text-center text-white" style="background-color: #f1f1f1">
+      <div class="container">
+        <section>
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-facebook-f"></i
+          ></a>
+
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-twitter"></i
+          ></a>
+
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-google"></i
+          ></a>
+
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-instagram"></i
+          ></a>
+
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-linkedin"></i
+          ></a>
+
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-github"></i
+          ></a>
+        </section>
+      </div>
+      <div
+        class="text-center text-dark p-3"
+        style="background-color: rgba(0, 0, 0, 0.2)"
+      >
+        © 2024 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">INF 3911 A</a>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="login">
+  <div>
     <LoginComp />
+    <FooterLayout/>
   </div>
 </template>
 
 <script>
 import LoginComp from "@/components/LoginComp.vue";
+import FooterLayout from "@/components/FooterLayout.vue";
 export default {
   components: {
     LoginComp,
+    FooterLayout
   }
 }
 </script>
